83d70b4619f45315541f815705056b1babec8415
urc4 <121357043+urc4@users.noreply.github.com>
2023-03-20 16:59:28 -0300

    Fix exercise 3

--- exercicio-03.py
+++ exercicio-03.py
@@ -1,14 +1,7 @@
-# def sum_to(n):
-#     """Find the sum of all integres up to and including n by iterative means"""
-#     sum = 0
-#     for k in range(n+1):
-#         sum = sum + k
-#     return sum
-
-
 def sum_to(n):
-   """Find the sum of all integres up to and including n by mathematical means"""
+   """Find the sum of all integres up to and including n 
+   using the formula for an arithmetic progression's sum"""
    return n*(n+1)/2
 
-
-print(int(sum_to(10)))
+n = 10
+print(int(sum_to(n)))
