37296f257cdab1329e665cfdae943a8bce889c99
urc4 <121357043+urc4@users.noreply.github.com>
2023-03-20 16:54:02 -0300

    Implement algorithm using left() method

--- exercicio-02.py
+++ exercicio-02.py
@@ -1,25 +1,21 @@
 import turtle
-import math
+
 def draw_poly(t,n,sz):
-    """Make turtle turtle_name draw a polygon of n sides each with lenght sz from the origin"""
-    pos_x = 0;
-    pos_y = 0;
-    angle_step = 2*math.pi/n
-    ang = 0
+    """Make turtle turtle t draw a polygon of n sides 
+    each with lenght sz starting from the origin"""
+    ang_step = 360/n 
+
     t.pensize(3)
     t.pencolor("orange")
-    for i in range(n):
-      ang = ang + angle_step
-      pos_x = pos_x + math.cos(ang)*sz
-      pos_y = pos_y + math.sin(ang)*sz
-      t.goto(pos_x,pos_y)
-      # could do this by using left
 
+    for i in range(n):
+      t.forward(sz)
+      t.left(ang_step)
 
 
 window = turtle.Screen()
 window.bgcolor("lightgreen")
 window.title("Exercicio 2")
 donatello = turtle.Turtle()
-draw_poly(donatello,7,100)
+draw_poly(donatello,5,100)
 window.mainloop()
