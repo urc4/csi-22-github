25d6c92d12cd6550ed028a9bf04b286fa2f364fb
urc4 <121357043+urc4@users.noreply.github.com>
2023-03-20 17:08:06 -0300

    Improve readability exercise 4

--- exercicio-04.py
+++ exercicio-04.py
@@ -1,25 +1,27 @@
 import turtle
 
-def draw_spiral(_turtle,step_angle,number_spirals):
-    """Make turtle _turtle draw a spiral of length by rotating it by a step angle"""    
-    length = 5
-    for i in range(number_spirals*4):
-        _turtle.forward(length)
-        _turtle.left(step_angle)
-        length = length + 5
+def draw_spiral(t,turn_ang,num_spirals):
+    """Make turtle t draw num_spirals of base size sz incremented every step
+    by rotating it by a step angle turn_ang"""    
+    t.pensize(3)
+    t.pencolor("blue")
+
+    sz = 5
+    step = 5
+
+    for i in range(num_spirals*4):
+        t.forward(sz)
+        t.left(turn_ang)
+        sz = sz + step
     
 
 wn = turtle.Screen();
 wn.bgcolor("lightgreen")
-wn.title("_turtle meets a function")
+wn.title("Exercicio 4")
 leonardo= turtle.Turtle()
-leonardo.pensize(3)
-leonardo.pencolor("blue")
-step_angle = 90
-step_angle = 91
-number_spirals = 100
-draw_spiral(leonardo,step_angle,number_spirals)
+turn_ang = 90
+turn_ang = 91
+num_spirals = 20
+draw_spiral(leonardo,turn_ang,num_spirals)
 
 wn.mainloop()
-
-# 
