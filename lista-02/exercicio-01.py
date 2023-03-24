40694bd81cb747725b09e40aaaad518a5b02395e
urc4 <121357043+urc4@users.noreply.github.com>
2023-03-23 23:55:56 -0300

    Finish lista 2 exercicio 1

---
+++ lista-02/exercicio-01.py
@@ -0,0 +1,40 @@
+def remove_empty_tuples(_tuple):
+    """Removes empty tuples"""
+    _list = list(_tuple)
+    empty_indexes = []
+
+    for index, tup in enumerate(_list):
+        if len(tup) == 0:
+            empty_indexes.append(index)
+
+    fix_index = 0
+
+    for index in empty_indexes:
+        _list.pop(index - fix_index)
+        fix_index += 1
+
+    return tuple(_list)
+
+
+_tuple = (
+    (),
+    (3, 2),
+    (2, 1, 4, 5),
+    (),
+    (),
+    (3, 4, 5, 6, 7, 8, 9, 10, 1),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+    (2, 1),
+)
+# does python pass by reference
+new_tuple = remove_empty_tuples(_tuple)
+print(_tuple)
+print(new_tuple)
